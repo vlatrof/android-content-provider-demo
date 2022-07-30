35cafd1d03cb700081f3f114455bb809e72e83b0
vlatrof <gthbttr@gmail.com>
2022-07-30 16:20:51 +0300

    dev

--- app/src/main/java/com/example/androidcontentproviderdemo/MainActivity.kt
+++ app/src/main/java/com/example/androidcontentproviderdemo/MainActivity.kt
@@ -15,6 +15,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
 
         val button = findViewById<Button>(R.id.btn_get_contacts)
